53b90869adabd37b759e12c3fe58866f611a07e8
Timo Mätäsaho <timo.matasaho@gmail.com>
2019-08-30 16:30:34 +0300

    Initialized the project to ES6. Added scripts.

--- src/bin/www.js
+++ src/bin/www.js
@@ -4,9 +4,10 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('rosterserver:server');
-var http = require('http');
+import app from '../app';
+import debugLib from 'debug';
+import http from 'http';
+const debug = debugLib('rosterserver:server');
 
 /**
  * Get port from environment and store in Express.
