80dc71a4b718c665c403256a628033fb1b2d9b25
Timo Mätäsaho <timo.matasaho@gmail.com>
2020-05-05 12:48:48 +0300

    Options to config. sessionOptions as function so they can be dynamically changed depending on environment

--- src/config/sessionOptions.ts
+++ src/config/sessionOptions.ts
@@ -3,10 +3,11 @@ import { createHash, randomBytes } from 'crypto';
 import { KnexSessionStore } from 'connect-session-knex';
 
 const sessionOptions = (store: KnexSessionStore, isDev: boolean, secret?: string) => {
+  const domain = isDev ? '127.0.0.1:4500' : 'NOT KNOWN YET';
   return {
     secret: secret || 'super-secret',
     store,
-    resave: false,
+    resave: true,
     saveUninitialized: false,
     // Generate unique ids that does not collide
     genid: () => createHash('sha256').update(uuidv1()).update(randomBytes(256)).digest('hex'),
@@ -14,6 +15,8 @@ const sessionOptions = (store: KnexSessionStore, isDev: boolean, secret?: string
       // One week
       maxAge: 1000*3600*24*7,
       secure: !isDev,
+      httpOnly: !isDev,
+      domain
     }
   };
 };
